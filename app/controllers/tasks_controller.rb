7cfe9308a255d6c128897473806fcd3b7cfd6df9
hatim ourrach <ourrachhatim0@gmail.com>
2024-01-16 14:37:44 +0100

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,40 @@
 class TasksController < ApplicationController
+  def home
+    @tasks = Task.all()
+  end
+
+  def create
+    title = params[:title]
+    details = params[:details]
+    @task = Task.create(title: title, details: details)
+    @task.save()
+    redirect_to "/"
+  end
+
+  def deleted
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    # redirect_to :action => "show", :id => @task[:id]
+  end
+
+  def delete
+    @task = Task.destroy(params[:id])
+    redirect_to "/"
+
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to "/"
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  root to: "tasks#home"
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks/:id", to: "tasks#show"
+  delete "tasks/:id", to: "tasks#delete"
+  post "create", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+
 end
